5eff90f2fa9d396401432e2b1b12ddfadb39252e
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-22 16:24:35 +0700

    Create Point.java

---
+++ JAVA OOP/J04010 - DIỆN TÍCH HÌNH TRÒN NGOẠI TIẾP TAM GIÁC/Point.java
@@ -0,0 +1,23 @@
+public class Point {
+    private double x;
+    private double y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double Distance(Point other) {
+        return Math.sqrt((other.x - this.x) * (other.x - this.x) + (other.y - this.y) * (other.y - this.y));
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+}
