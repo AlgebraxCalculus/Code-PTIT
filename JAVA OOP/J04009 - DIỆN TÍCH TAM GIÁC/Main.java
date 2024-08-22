e94261c4fcbff12184c253132ab644a85f521e62
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-22 16:22:58 +0700

    Create Main.java

---
+++ JAVA OOP/J04009 - DIỆN TÍCH TAM GIÁC/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t --> 0) {
+            Point a = new Point(sc.nextDouble(), sc.nextDouble());
+            Point b = new Point(sc.nextDouble(), sc.nextDouble());
+            Point c = new Point(sc.nextDouble(), sc.nextDouble());
+            double d1 = a.Distance(b);
+            double d2 = b.Distance(c);
+            double d3 = a.Distance(c);
+            if (d1 + d2 <= d3 || d1 + d3 <= d2 || d2 + d3 <= d1) {
+                System.out.println("INVALID");
+            } else {
+                double p = (d1 + d2 + d3) / 2;
+                double s = Math.sqrt(p * (p - d1) * (p - d2) * (p - d3));
+                System.out.printf("%.2f\n", s);
+            }
+        }
+    }
+}
