b52ebeb1a374eed119f2af8722a9aa7524fb8188
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-24 15:03:08 +0700

    Create Main.java

---
+++ JAVA OOP/J07045 - LOẠI PHÒNG/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ArrayList<LoaiPhong> ds = new ArrayList<>();
+        Scanner in = new Scanner(new File("PHONG.in"));
+        int n = Integer.parseInt(in.nextLine());
+        while(n --> 0){
+            ds.add(new LoaiPhong(in.nextLine()));
+        }
+        Collections.sort(ds);
+        for(LoaiPhong tmp : ds){
+            System.out.println(tmp);
+        }
+    }
+}
