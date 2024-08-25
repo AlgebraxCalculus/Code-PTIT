3ed49197b8225ad9356e94cbe26975d67880af92
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-25 22:26:59 +0700

    Create Main.java

---
+++ JAVA OOP/J04011 - BỐN ĐIỂM TRÊN MẶT PHẲNG/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t --> 0) {
+        Point3D p1 = new Point3D(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        Point3D p2 = new Point3D(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        Point3D p3 = new Point3D(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        Point3D p4 = new Point3D(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        if (Point3D.check(p1,p2,p3,p4)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+          }
+        }
+    }
+}
