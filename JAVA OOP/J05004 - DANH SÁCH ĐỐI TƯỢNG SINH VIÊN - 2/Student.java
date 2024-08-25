15d4892697184a1801d294adc72f5c7021f64e93
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-25 22:20:48 +0700

    Create Student.java

---
+++ JAVA OOP/J05004 - DANH SÁCH ĐỐI TƯỢNG SINH VIÊN - 2/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    private static int cnt = 1;
+    private String ma, ten, lop, dob;
+    private float gpa;
+    public Student (String ten, String lop, String dob, float gpa) {
+        this.ma = String.format("B20DCCN%03d", cnt++);
+        this.ten = nameFormat(ten);
+        this.lop = lop;
+        this.dob = dobFormat(dob);
+        this.gpa = gpa;
+    }
+    public String nameFormat (String s) {
+        String[] name = s.trim().toLowerCase().split("\\s+");
+        String res = "";
+        for (String x : name) {
+            res += x.substring(0, 1).toUpperCase() + x.substring(1) + " ";
+        }
+        return res;
+    }
+    public String dobFormat (String s) {
+        StringBuilder ss = new StringBuilder(s);
+        if (ss.charAt(1) == '/') {
+            ss.insert(0, '0');
+        }
+        if (ss.charAt(4) == '/') {
+            ss.insert(3, '0');
+        }
+        return ss.toString();
+    }
+    @Override
+    public String toString() {
+        return ma + " " + ten + lop + " " + dob + " " + String.format("%.2f", gpa);
+    }
+}
