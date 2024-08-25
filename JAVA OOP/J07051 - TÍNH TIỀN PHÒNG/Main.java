9aa85a7abd0527d22ccd9e57ec398565154bd19e
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-25 22:31:36 +0700

    Create Main.java

---
+++ JAVA OOP/J07051 - TÍNH TIỀN PHÒNG/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("KHACHHANG.in"));
+            int n = Integer.parseInt(sc.nextLine());
+            ArrayList<Customer> a = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                a.add(new Customer(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(), Integer.parseInt(sc.nextLine())));
+            }
+            Collections.sort(a);
+            for (Customer i : a) {
+                System.out.println(i);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
