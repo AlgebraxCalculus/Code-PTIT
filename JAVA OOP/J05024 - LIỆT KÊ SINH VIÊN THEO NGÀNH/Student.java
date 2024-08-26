685908a85f5dbf6f187fdb7612d77d3c5fbd5a1d
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-26 21:55:47 +0700

    Create Student.java

---
+++ JAVA OOP/J05024 - LIỆT KÊ SINH VIÊN THEO NGÀNH/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String ma, ten, lop, email;
+    public Student (String ma, String ten, String lop, String email) {
+        this.ma = ma;
+        this.ten = ten;
+        this.lop = lop;
+        this.email = email;
+    }
+    public String getMa() {
+        return ma;
+    }
+    public String getLop() {
+        return lop;
+    }
+    @Override
+    public String toString() {
+        return ma + " " + ten + " " + lop + " " + email;
+    }
+}
