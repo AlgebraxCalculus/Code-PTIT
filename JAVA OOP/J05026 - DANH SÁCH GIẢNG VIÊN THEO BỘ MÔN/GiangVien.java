1b7ad00ab10ac2ce7b01c5c31a48d882eb793fde
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-27 21:12:49 +0700

    Create GiangVien.java

---
+++ JAVA OOP/J05026 - DANH SÁCH GIẢNG VIÊN THEO BỘ MÔN/GiangVien.java
@@ -0,0 +1,24 @@
+public class GiangVien {
+    private static int cnt = 1;
+    private String ma, ten, bomon;
+    public GiangVien(String ten, String bomon) {
+        this.ma = String.format("GV%02d", cnt++);
+        this.ten = ten;
+        this.bomon = chuanhoa(bomon);
+    }
+    public String chuanhoa (String s) {
+        String[] ss = s.split("\\s+");
+        String res = "";
+        for (String i : ss) {
+            res += i.substring(0, 1).toUpperCase();
+        }
+        return res;
+    }
+    public String getBomon() {
+        return bomon;
+    }
+    @Override
+    public String toString() {
+        return ma + " " + ten + " " + bomon;
+    }
+}
