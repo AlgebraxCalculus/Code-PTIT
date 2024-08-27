a9592a47945f2ffed38a5f33b1d82d7347fa31c5
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-27 21:23:31 +0700

    Create Main.java

---
+++ JAVA OOP/J05030 - BẢNG ĐIỂM THÀNH PHẦN - 1/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        BangDiem[] a = new BangDiem[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = new BangDiem(sc.nextLine(), sc.nextLine(), sc.nextLine(), Double.parseDouble(sc.nextLine()), Double.parseDouble(sc.nextLine()), Double.parseDouble(sc.nextLine()));
+        }
+        Arrays.sort(a, new Comparator<BangDiem>() {
+            @Override
+            public int compare(BangDiem o1, BangDiem o2) {
+                return o1.getMa().compareTo(o2.getMa());
+            }
+        });
+        for (BangDiem x : a) {
+            System.out.println(x);
+        }
+    }
+}
