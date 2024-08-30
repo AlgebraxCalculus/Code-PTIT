44c17e782653fb6e7e2e2681ef79c10f77f0ef43
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-30 16:39:33 +0700

    Create Main.java

---
+++ JAVA OOP/J05011 - TÍNH GIỜ/J05011 - TÍNH GIỜ/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        ArrayList<OnlineTime> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add(new OnlineTime(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine()));
+        }
+        Collections.sort(a);
+        for (OnlineTime x : a) {
+            System.out.println(x);
+        }
+    }
+}
