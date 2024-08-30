e88a60e76304a8266834ee12bfb8907dba4a07af
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-30 17:06:55 +0700

    Create Main.java

---
+++ JAVA OOP/J05046 - BẢNG KÊ NHẬP KHO/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        ArrayList<Product> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add(new Product(sc.nextLine(), Integer.parseInt(sc.nextLine()), Integer.parseInt(sc.nextLine())));
+        }
+        for (Product x : a) {
+            System.out.println(x);
+        }
+    }
+}
