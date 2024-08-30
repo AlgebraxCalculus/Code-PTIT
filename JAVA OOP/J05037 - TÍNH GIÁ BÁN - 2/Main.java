94d157c854f8aeef3a008cd55bb1bff08c75effd
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-30 16:44:11 +0700

    Create Main.java

---
+++ JAVA OOP/J05037 - TÍNH GIÁ BÁN - 2/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        ArrayList<Product> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add(new Product(sc.nextLine(), sc.nextLine(), Integer.parseInt(sc.nextLine()), Integer.parseInt(sc.nextLine())));
+        }
+        Collections.sort(a);
+        for (Product x : a) {
+            System.out.println(x);
+        }
+    }
+}
