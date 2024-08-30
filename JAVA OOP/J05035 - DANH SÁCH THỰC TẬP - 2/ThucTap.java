9e975685f8a006ddb55018f85ce7f11c3ca5bb25
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-30 16:32:55 +0700

    Create ThucTap.java

---
+++ JAVA OOP/J05035 - DANH SÁCH THỰC TẬP - 2/ThucTap.java
@@ -0,0 +1,23 @@
+public class ThucTap implements Comparable<ThucTap> {
+    private static int cnt = 1;
+    private String stt, ma, ten, lop, email, dn;
+    public ThucTap (String ma, String ten, String lop, String email, String dn) {
+        this.stt = String.format("%s", cnt++);
+        this.ma = ma;
+        this.ten = ten;
+        this.lop = lop;
+        this.email = email;
+        this.dn = dn;
+    }
+    public String getDn() {
+        return dn;
+    }
+    @Override
+    public int compareTo(ThucTap o) {
+        return ma.compareTo(o.ma);
+    }
+    @Override
+    public String toString() {
+        return stt + " " + ma + " " + ten + " " + lop + " " + email + " " + dn;
+    }
+}
