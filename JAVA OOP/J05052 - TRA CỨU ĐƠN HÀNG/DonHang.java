ea832a25bf335e088c6216fcf6953442efa69dd8
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-31 22:25:56 +0700

    Create DonHang.java

---
+++ JAVA OOP/J05052 - TRA CỨU ĐƠN HÀNG/DonHang.java
@@ -0,0 +1,24 @@
+public class DonHang {
+    private String ten, ma, stt;
+    private int dongia, sl, giamgia, thanhtien;
+    public DonHang (String ten, String ma, int dongia, int sl) {
+        this.ten = ten;
+        this.ma = ma;
+        this.dongia = dongia;
+        this.sl = sl;
+        this.stt = ma.substring(1, 4);
+        this.giamgia = getGiamgia();
+        this.thanhtien = dongia * sl - giamgia;
+    }
+    public int getGiamgia() {
+        if (ma.charAt(4) == '1') {
+            return (int) Math.round(dongia * sl * 50.0 / 100);
+        } else {
+            return (int) Math.round(dongia * sl * 30.0 / 100);
+        }
+    }
+    @Override
+    public String toString() {
+        return ten + " " + ma + " " + stt + " " + giamgia + " " + thanhtien;
+    }
+}
