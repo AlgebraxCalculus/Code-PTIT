8f0beb42c50f984c37659ab0009cd4b3b14c3628
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-08-31 22:23:20 +0700

    Create Product.java

---
+++ JAVA OOP/J05049 - LIỆT KÊ NHẬP XUẤT HÀNG THEO NHÓM/Product.java
@@ -0,0 +1,51 @@
+public class Product implements Comparable<Product> {
+    private String ma;
+    private int sl_nhap, sl_xuat, don_gia, tien, thue;
+    public Product (String ma, int sl_nhap) {
+        this.ma = ma;
+        this.sl_nhap = sl_nhap;
+        this.sl_xuat = getSl_xuat();
+        this.don_gia = getDon_gia();
+        this.tien = sl_xuat * don_gia;
+        this.thue = getThue();
+    }
+    public String getMa() {
+        return ma;
+    }
+    public int getSl_xuat() {
+        if (ma.charAt(0) == 'A') {
+            return (int) Math.round(0.6 * sl_nhap);
+        } else {
+            return (int) Math.round(0.7 * sl_nhap);
+        }
+    }
+    public int getDon_gia() {
+        if (ma.charAt(4) == 'Y') {
+            return 110000;
+        }
+        return 135000;
+    }
+    public int getThue() {
+        if (ma.charAt(0) == 'A') {
+            if (ma.charAt(4) == 'Y') {
+                return (int) Math.round(0.08 * tien);
+            } else {
+                return (int) Math.round(0.11 * tien);
+            }
+        } else {
+            if (ma.charAt(4) == 'Y') {
+                return (int) Math.round(0.17 * tien);
+            } else {
+                return (int) Math.round(0.22 * tien);
+            }
+        }
+    }
+    @Override
+    public int compareTo(Product o) {
+        return o.thue - thue;
+    }
+    @Override
+    public String toString() {
+        return ma + " " + sl_nhap + " " + sl_xuat + " " + don_gia + " " + tien + " " + thue;
+    }
+}
