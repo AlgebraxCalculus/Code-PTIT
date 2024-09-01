fe2381fd1fa05ff1553367a21e3d572f088f5067
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-01 16:43:54 +0700

    Create Main.java

---
+++ JAVA OOP/J05069 - CÂU LẠC BỘ BÓNG ĐÁ - 1/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Club[] a = new Club[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = new Club(sc.nextLine(), sc.nextLine(), Integer.parseInt(sc.nextLine()));
+        }
+        int m = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < m; i++) {
+            String[] ss = sc.nextLine().split("\\s+");
+            String ma = ss[0];
+            int sl_cdv = Integer.parseInt(ss[1]);
+            String tendoi = "";
+            int gia_ve = 0;
+            for (int j = 0; j < n; j++) {
+                if (ma.substring(1, 3).equals(a[j].getMa())) {
+                    tendoi = a[j].getTen();
+                    gia_ve = a[j].getGiave();
+                    break;
+                }
+            }
+            System.out.println(ma + " " + tendoi + " " + sl_cdv * gia_ve);
+        }
+    }
+}
