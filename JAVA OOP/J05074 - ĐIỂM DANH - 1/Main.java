4a109e1aa83452d744d08529f31ff1cd1083b01a
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-01 16:45:15 +0700

    Create Main.java

---
+++ JAVA OOP/J05074 - ĐIỂM DANH - 1/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        ArrayList<Student> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add(new Student(sc.nextLine(), sc.nextLine(), sc.nextLine()));
+        }
+        for (int i = 0; i < n; i++) {
+            String[] ss = sc.nextLine().split("\\s+");
+            String ma = ss[0];
+            String cc = ss[1];
+            for (Student x : a) {
+                if (x.getMa().equals(ma)) {
+                    x.setDiem(cc);
+                }
+            }
+        }
+        for (Student x : a) {
+            System.out.println(x);
+        }
+    }
+}
