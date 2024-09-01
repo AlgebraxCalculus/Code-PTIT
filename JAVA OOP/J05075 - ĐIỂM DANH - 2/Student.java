ff6df57e6c9a4edb57ef3b2ac5c0f51b5dc7cad2
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-01 16:46:19 +0700

    Create Student.java

---
+++ JAVA OOP/J05075 - ĐIỂM DANH - 2/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    private String ma, ten, lop;
+    private int diem;
+    public Student (String ma, String ten, String lop) {
+        this.ma = ma;
+        this.ten = ten;
+        this.lop = lop;
+    }
+    public String getMa() {
+        return ma;
+    }
+    public String getLop() {
+        return lop;
+    }
+    public void setDiem(String s) {
+        int cntM = 0, cntV = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'm') {
+                cntM++;
+            } else if (s.charAt(i) == 'v') {
+                cntV++;
+            }
+        }
+        this.diem = Math.max(0, 10 - cntM - (cntV * 2));
+    }
+    @Override
+    public String toString() {
+        if (diem == 0) {
+            return ma + " " + ten + " " + lop + " " + diem + " KDDK";
+        } else {
+            return ma + " " + ten + " " + lop + " " + diem;
+        }
+    }
+}
