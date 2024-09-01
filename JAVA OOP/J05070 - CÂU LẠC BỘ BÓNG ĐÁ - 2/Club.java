0edbc815c4a7cb624b4dbe1f5219ba5ff763dd3b
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-01 16:44:16 +0700

    Create Club.java

---
+++ JAVA OOP/J05070 - CÂU LẠC BỘ BÓNG ĐÁ - 2/Club.java
@@ -0,0 +1,18 @@
+public class Club {
+    private String ma, ten;
+    private int giave;
+    public Club (String ma, String ten, int giave) {
+        this.ma = ma;
+        this.ten = ten;
+        this.giave = giave;
+    }
+    public String getMa() {
+        return ma;
+    }
+    public String getTen() {
+        return ten;
+    }
+    public int getGiave() {
+        return giave;
+    }
+}
