046b1cdf9811922dd1ea84e0ce77bd3933128f1b
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-02 15:19:49 +0700

    Create Main.java

---
+++ JAVA OOP/J07013 - DANH SÁCH SINH VIÊN TRONG FILE NHỊ PHÂN/Main.java
@@ -0,0 +1,16 @@
+package danhsachsinhvien1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ObjectInputStream inp = new ObjectInputStream(new FileInputStream("SV.in"));
+        ArrayList<SinhVien> a = (ArrayList<SinhVien>) inp.readObject();
+        for (SinhVien i : a) {
+            System.out.println(i);
+        }
+    }
+}
