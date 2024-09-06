969de6ef98d107965bffe646b16d681cbff40468
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-06 21:24:46 +0700

    Create MonHoc.java

---
+++ JAVA OOP/J07028 - TÍNH GIỜ CHUẨN/MonHoc.java
@@ -0,0 +1,13 @@
+public class MonHoc {
+    private String ma, ten;
+    public MonHoc (String ma, String ten) {
+        this.ma = ma;
+        this.ten = ten.trim();
+    }
+    public String getMa() {
+        return ma;
+    }
+    public String getTen() {
+        return ten;
+    }
+}
