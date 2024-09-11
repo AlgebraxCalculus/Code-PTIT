d2b5c4c1f7203c0d12f209037bc49606869b6a5a
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-11 09:29:54 +0700

    Create Main.java

---
+++ JAVA OOP/TN02012 - TÍNH LƯƠNG/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Map<String, String> mp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String ma_pb = sc.next();
+            String ten_pb = sc.nextLine();
+            mp.put(ma_pb, ten_pb);
+        }
+        int m = Integer.parseInt(sc.nextLine());
+        Employee[] a = new Employee[m];
+        for (int i = 0; i < m; i++) {
+            a[i] = new Employee(sc.nextLine(), sc.nextLine(), Long.parseLong(sc.nextLine()), Long.parseLong(sc.nextLine()));
+            if (mp.containsKey(a[i].getMa_phong())) {
+                a[i].setTen_phong(mp.get(a[i].getMa_phong()));
+            }
+        }
+        for (Employee x : a) {
+            System.out.println(x);
+        }
+    }
+}
