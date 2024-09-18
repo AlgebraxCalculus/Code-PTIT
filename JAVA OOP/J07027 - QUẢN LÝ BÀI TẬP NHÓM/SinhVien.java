f171c2e739333382494bdfdb991a2bfc7c8a0aa9
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-18 09:52:15 +0700

    Create SinhVien.java

---
+++ JAVA OOP/J07027 - QUẢN LÝ BÀI TẬP NHÓM/SinhVien.java
@@ -0,0 +1,17 @@
+public class SinhVien {
+    private String ma, ten, sdt;
+    public SinhVien (String ma, String ten, String sdt) {
+        this.ma = ma;
+        this.ten = ten;
+        this.sdt = sdt;
+    }
+    public String getMa() {
+        return ma;
+    }
+    public String getTen() {
+        return ten;
+    }
+    public String getSdt() {
+        return sdt;
+    }
+}
