8e5749924a0232498796e27c697fe275aeeab94e
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-18 09:51:27 +0700

    Create Main.java

---
+++ JAVA OOP/J07036 - BẢNG ĐIỂM THEO LỚP/Main.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("SINHVIEN.in"));
+            int n = Integer.parseInt(sc.nextLine());
+            SinhVien[] a = new SinhVien[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = new SinhVien(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine());
+            }
+            sc = new Scanner(new File("MONHOC.in"));
+            int m = Integer.parseInt(sc.nextLine());
+            MonHoc[] b = new MonHoc[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = new MonHoc(sc.nextLine(), sc.nextLine(), Integer.parseInt(sc.nextLine()));
+            }
+            sc = new Scanner(new File("BANGDIEM.in"));
+            int p = sc.nextInt();
+            ArrayList<BangDiem> c = new ArrayList<>();
+            for (int i = 0; i < p; i++) {
+                c.add(new BangDiem(sc.next(), sc.next(), Float.parseFloat(sc.next())));
+                for (SinhVien x : a) {
+                    if (x.getMa().equals(c.get(i).getMaSV())) {
+                        c.get(i).setTenSV(x.getTen());
+                        c.get(i).setLopSV(x.getLop());
+                    }
+                }
+                for (MonHoc y : b) {
+                    if (y.getMa().equals(c.get(i).getMaMon())) {
+                        c.get(i).setTenMon(y.getTen());
+                    }
+                }
+            }
+            Collections.sort(c);
+            int q = sc.nextInt();
+            while (q --> 0) {
+                String s = sc.next();
+                System.out.println("BANG DIEM lop " + s + ":");
+                for (BangDiem x : c) {
+                    if (x.getLopSV().equals(s)) {
+                        System.out.println(x);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
