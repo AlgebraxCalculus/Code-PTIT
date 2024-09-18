00dbf813f5e696b510ee21c77f72eb99a36fc4b2
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-18 09:48:17 +0700

    Create Main.java

---
+++ JAVA OOP/J05016 - HÓA ĐƠN KHÁCH SẠN/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        ArrayList<Customer> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add(new Customer(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(), Long.parseLong(sc.nextLine())));
+        }
+        Collections.sort(a);
+        for (Customer x : a) {
+            System.out.println(x);
+        }
+    }
+}
