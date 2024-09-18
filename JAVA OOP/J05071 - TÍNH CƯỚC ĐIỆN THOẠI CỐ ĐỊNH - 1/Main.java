b1a60ca3dc6bcea04dc65ce1c0f0a1622fc518bc
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-18 09:40:03 +0700

    Create Main.java

---
+++ JAVA OOP/J05071 - TÍNH CƯỚC ĐIỆN THOẠI CỐ ĐỊNH - 1/Main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        ThanhPho[] a = new ThanhPho[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = new ThanhPho(sc.nextLine(), sc.nextLine(), Integer.parseInt(sc.nextLine()));
+        }
+        int m = Integer.parseInt(sc.nextLine());
+        CuocGoi[] b = new CuocGoi[m];
+        for (int i = 0; i < m; i++) {
+            String[] tmp = sc.nextLine().split("\\s+");
+            String so = tmp[0], batdau = tmp[1], ketthuc = tmp[2];
+            String s1 = so.substring(0, 1);
+            if (s1.equals("0")) {
+                String s2 = so.substring(1, 3);
+                for (ThanhPho x : a) {
+                    if (s2.equals(x.getMa())) {
+                        b[i] = new CuocGoi(so, x.getTen());
+                        b[i].setSophut(batdau, ketthuc);
+                        b[i].setTongcuoc(x.getGiacuoc());
+                    }
+                }
+            } else {
+                b[i] = new CuocGoi(so, "Noi mang");
+                b[i].setSophut(batdau, ketthuc);
+                b[i].setTongcuoc(800);
+            }
+        }
+        for (CuocGoi x : b) {
+            System.out.println(x);
+        }
+    }
+}
