66d1f0584903a45a31f4908163204359a3be40ef
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-18 09:51:47 +0700

    Create MonHoc.java

---
+++ JAVA OOP/J07036 - BẢNG ĐIỂM THEO LỚP/MonHoc.java
@@ -0,0 +1,15 @@
+public class MonHoc {
+    private String ma, ten;
+    private int so_tc;
+    public MonHoc (String ma, String ten, int so_tc) {
+        this.ma = ma;
+        this.ten = ten;
+        this.so_tc = so_tc;
+    }
+    public String getMa() {
+        return ma;
+    }
+    public String getTen() {
+        return ten;
+    }
+}
