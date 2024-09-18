0d5394d0a184ba5e5266d948a856b344d198ea5a
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-09-18 11:10:04 +0700

    Create Main.java

---
+++ JAVA OOP/J04016 - TÍCH HAI ĐỐI TƯỢNG MA TRẬN/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), p = sc.nextInt();
+        Matrix a = new Matrix(n,m);
+        a.nextMatrix(sc);
+        Matrix b = new Matrix(m,p);
+        b.nextMatrix(sc);
+        System.out.println(a.mul(b));
+    }
+}
