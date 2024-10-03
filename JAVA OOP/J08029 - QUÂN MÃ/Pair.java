a2ad1b424b37b22438554aed5e65c19aeee57af1
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-10-03 09:49:48 +0700

    Create Pair.java

---
+++ JAVA OOP/J08029 - QUÂN MÃ/Pair.java
@@ -0,0 +1,14 @@
+public class Pair {
+    private String first;
+    private int second;
+    public Pair (String first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+    public String getFirst() {
+        return first;
+    }
+    public int getSecond() {
+        return second;
+    }
+}
