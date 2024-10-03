489240d104f4a2c840ca7151903c702868ac58fc
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-10-03 09:44:04 +0700

    Create MonThi.java

---
+++ JAVA OOP/J07060 - SẮP XẾP LỊCH THI/MonThi.java
@@ -0,0 +1,14 @@
+public class MonThi {
+    private String maMon, tenMon, hinhthucthi;
+    public MonThi (String maMon, String tenMon, String hinhthucthi) {
+        this.maMon = maMon;
+        this.tenMon = tenMon;
+        this.hinhthucthi = hinhthucthi;
+    }
+    public String getMaMon() {
+        return maMon;
+    }
+    public String getTenMon() {
+        return tenMon;
+    }
+}
