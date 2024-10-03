819865227519cbfae8c59fd9804960a8332c67f7
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-10-03 09:48:05 +0700

    Create Pair.java

---
+++ JAVA OOP/J08026 - BIẾN ĐỔI S – T/Pair.java
@@ -0,0 +1,13 @@
+public class Pair {
+    private int first, second;
+    public Pair (int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+    public int getFirst() {
+        return first;
+    }
+    public int getSecond() {
+        return second;
+    }
+}
