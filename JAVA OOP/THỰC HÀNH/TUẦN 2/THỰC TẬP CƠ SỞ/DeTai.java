e8efbf2ee29607e2e3bb2d5abcb201295ca15046
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2024-11-20 08:56:34 +0700

    Create DeTai.java

---
+++ JAVA OOP/THỰC HÀNH/TUẦN 2/THỰC TẬP CƠ SỞ/DeTai.java
@@ -0,0 +1,18 @@
+public class DeTai {
+    private static int cnt = 1;
+    private String maDT, tenGV, tenDT;
+    public DeTai (String tenGV, String tenDT) {
+        this.maDT = String.format("DT%03d", cnt++);
+        this.tenGV = tenGV;
+        this.tenDT = tenDT;
+    }
+    public String getMaDT() {
+        return maDT;
+    }
+    public String getTenGV() {
+        return tenGV;
+    }
+    public String getTenDT() {
+        return tenDT;
+    }
+}
